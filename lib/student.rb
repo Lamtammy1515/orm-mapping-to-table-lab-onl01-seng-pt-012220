e7488ad888ccd3704bd24119bb25ba1849400a20
Tammy Lam <lamtammy1515@gmail.com>
2020-03-25 18:37:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
   insert into students (name,grade) values(?,?);
   SQL
   DB[:conn].execute(sql, self.name, self.grade)
-  @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
   def self.create(name:, grade:)
